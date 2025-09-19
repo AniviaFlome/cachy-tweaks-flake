4f92d9717d434c865b08cdf47c4d473c33e799a4
AniviaFlome <aniviaflome@gmail.com>
2025-09-19 19:59:38 +0300

    Small Changes

--- modules/cachy-tweaks.nix
+++ modules/cachy-tweaks.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 
 with lib;
 
@@ -13,4 +13,25 @@ with lib;
     ./zram.nix
     ./scripts.nix
   ];
+
+  options.cachy = {
+    enable = mkEnableOption "CachyOS tweaks";
+
+    all = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable all CachyOS tweaks";
+    };
+  };
+
+  config = {
+    cachy = {
+      kernel = mkIf config.cachy.all (mkDefault true);
+      udev = mkIf config.cachy.all (mkDefault true);
+      modprobe = mkIf config.cachy.all (mkDefault true);
+      systemd = mkIf config.cachy.all (mkDefault true);
+      xserver = mkIf config.cachy.all (mkDefault true);
+      scripts = mkIf config.cachy.all (mkDefault true);
+    };
+  };
 }
